8b452aa4a7801e368952006a1bc00ae4ae0607e2
juancalderon <juanestebancalderonovalle@gmail.com>
2024-10-16 14:06:30 -0500

    add link de git of the files

--- config.py
+++ config.py
@@ -3,6 +3,8 @@
 # Configuraciones generales del proceso ETL
 # SOURCE_DB_URL = "mssql+pyodbc://username:password@source_db?driver=ODBC+Driver+17+for+SQL+Server"
 # DEST_DB_URL = "mssql+pyodbc://username:password@dest_db?driver=ODBC+Driver+17+for+SQL+Server"
-
-API_URL = "https://github.com/sferez/BybitMarketData/raw/main/data/ETH/2024-02-12/trades_ETH_2024-02-12.zip"
+# https://github.com/sferez/BybitMarketData/raw/main/data/BTC/2024-02-12/trades_BTC_2024-02-12.zip
+# https://github.com/sferez/BybitMarketData/raw/main/data/ETH/2024-02-12/trades_ETH_2024-02-12.zip
+# https://github.com/sferez/BybitMarketData/raw/main/data/SOL/2024-02-12/trades_SOL_2024-02-12.zip
+API_URL = "https://github.com/sferez/BybitMarketData/raw/main/data/SOL/2024-02-12/trades_SOL_2024-02-12.zip"
 
